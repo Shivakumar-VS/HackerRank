1335879cd98f542d3ee29c92156c9900f8e2fcbd
Shivakumar-001 <shivagana2004@gmail.com>
2025-02-05 20:18:06 +0530

    Hackerrank Solution

--- sql/Top Earners.sql
+++ sql/Top Earners.sql
@@ -1,9 +1,2 @@
-SELECT 
-    MAX(months * salary) AS max_earnings,
-    COUNT(*) AS employee_count
-FROM 
-    Employee
-WHERE 
-    (months * salary) = (
-        SELECT MAX(months * salary) FROM Employee
-    );
+select months*salary as earnings, count(*) from Employee group by earnings order by earnings desc limit 1;
+
