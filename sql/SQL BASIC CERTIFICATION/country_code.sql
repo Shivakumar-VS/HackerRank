c3c99d05afd53f3dd2a1b7e892aae25d7fd923c9
Shivakumar-001 <shivagana2004@gmail.com>
2025-02-28 19:21:40 +0530

    Basic certification

---
+++ sql/SQL BASIC CERTIFICATION/country_code.sql
@@ -0,0 +1,6 @@
+--country code
+
+select a.customer_id,a.name,CONCAT("+",b.country_code,a.phone_number)
+ from customers as a
+left join country_codes as b on a.country=b.country
+order by a.customer_id;
